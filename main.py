3876dd4f0e8c0e32e2ab4e0cfe35627ff32b10e7
paul <pgtech246@gmail.com>
2024-03-05 14:01:23 -0400

    Remove print(df) statement

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
     filename = Path(filepath).stem
